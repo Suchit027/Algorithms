fefb5599a7a779ca4b4430625f2dee5b7f1d8f47
Suchit Gupta <suchitgupta027@gmail.com>
2024-02-26 19:33:01 +0530

    my implementation

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,21 +1,27 @@
-import heapq
-n = 4
-k = 2
-times = [[2,1,1],[2,3,1],[3,4,1]]
-adList = [[] for i in range(n)]
-for i in times:
-    adList[i[0] - 1].append((i[2], i[1] - 1))
-queue = [(0, k - 1)]
-dis = [1000000] * n
-visit = []
-while queue:
-    curr = heapq.heappop(queue)
-    if curr[1] not in visit:
-        visit.append(curr[1])
-        for x in adList[curr[1]]:
-            if x[0] + dis[curr[1]] < dis[x[1]]:
-                dis[x[1]] = dis[curr[1]] + x[0]
-                heapq.heappush(queue, (dis[x[1]], x[1]))
-if 1000000 in dis:
-    print(-1)
-print(max(dis))
+def isfence(alice, bob):
+    if alice[0] <= bob[0] and alice[1] >= bob[1]:
+        return True
+    return False
+
+
+def numberOfPairs(points):
+    length = len(points)
+    ans = 0
+    for i in range(length - 1):
+        for j in range(i + 1, length):
+            if isfence(points[i], points[j]):
+                flag = True
+                for x in points:
+                    if x == points[i] or x == points[j]:
+                        continue
+                    if points[i][0] <= x[0] <= points[j][0] and points[i][1] >= x[1] >= points[j][1]:
+                        flag = False
+                        break
+                if flag:
+                    ans += 1
+    return ans
+
+
+if __name__ == '__main__':
+    points = [[6, 2], [4, 4], [2, 6]]
+    print(numberOfPairs(points))
