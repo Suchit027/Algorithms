86706ca8e9f16668ae4a4b71b2653f620eb9061c
Suchit Gupta <suchitgupta027@gmail.com>
2024-03-01 21:32:40 +0530

    a* algo

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,27 +1,33 @@
-def isfence(alice, bob):
-    if alice[0] <= bob[0] and alice[1] >= bob[1]:
-        return True
-    return False
-
-
-def numberOfPairs(points):
-    length = len(points)
-    ans = 0
-    for i in range(length - 1):
-        for j in range(i + 1, length):
-            if isfence(points[i], points[j]):
-                flag = True
-                for x in points:
-                    if x == points[i] or x == points[j]:
-                        continue
-                    if points[i][0] <= x[0] <= points[j][0] and points[i][1] >= x[1] >= points[j][1]:
-                        flag = False
-                        break
-                if flag:
-                    ans += 1
+def trydel(nums, target, ans):
+    a, b, c, = 0, 0, 0
+    if len(nums) < 2:
+        return ans
+    if sum(nums[:2]) == target:
+        a = trydel(nums[2:], target, ans + 1)
+    if nums[-1] + nums[-2] == target:
+        b = trydel(nums[: len(nums) - 2], target, ans + 1)
+    if nums[0] + nums[-1] == target:
+        c = trydel(nums[1: len(nums) - 1], target, ans + 1)
+    if a > b and a > c and a != 0:
+        ans = a
+    elif b > a and b > c and b != 0:
+        ans = b
+    elif c != 0:
+        ans = c
     return ans
 
 
-if __name__ == '__main__':
-    points = [[6, 2], [4, 4], [2, 6]]
-    print(numberOfPairs(points))
+def maxOperations(nums):
+    if len(nums) < 2:
+        return 0
+    a = trydel(nums[2:], sum(nums[: 2]), 1)
+    b = trydel(nums[: len(nums) - 2], nums[-1] + nums[-2], 1)
+    c = trydel(nums[1: len(nums) - 1], nums[0] + nums[-1], 1)
+    if a > b and a > c:
+        return a
+    if b > a and b > c:
+        return b
+    return c
+
+
+print(maxOperations([5, 2, 1, 6, 3, 4, 4]))
