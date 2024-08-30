d580115c16d74a3b08f4eadcff350cb9b168f213
Suchit Gupta <suchitgupta027@gmail.com>
2024-08-30 21:50:50 +0530

    commit

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,35 +1,36 @@
-def islandPerimeter(grid):
-    store = {1: 0, 2: 0, 3: 0}
-    m, n = len(grid), len(grid[0])
+def countSubIslands(grid1, grid2):
+    direc = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    ans = 0
     visit = set()
-    direc = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-
-    def dfs(i, j):
+    m, n = len(grid1), len(grid1[0])
+    def helper(i, j):
         visit.add((i, j))
-        surr = 0
-        if i > 0:
-            surr = grid[i - 1][j]
-        if i < m - 1:
-            surr += grid[i + 1][j]
-        if j > 0:
-            surr += grid[i][j - 1]
-        if j < n - 1:
-            surr += grid[i][j + 1]
-        if surr < 4:
-            store[surr] += 1
+        ret = True
         for x, y in direc:
             a = i + x
             b = j + y
-            if 0 <= a < m and 0 <= b < n and (a, b) not in visit and grid[a][b] == 1:
-                dfs(a, b)
-        return
+            if 0 <= a < m and 0 <= b < n and grid2[a][b] == 1 and (a, b) not in visit:
+                if grid1[a][b] == 0:
+                    helper(a, b)
+                    ret = False
+                else:
+                    ret = ret & helper(a, b)
+        return ret
 
     for i in range(m):
         for j in range(n):
-            if grid[i][j] == 1:
-                dfs(i, j)
-                break
-    ans = (store[1] * 3) + (store[2] * 2) + (store[3])
+            if (i, j) in visit:
+                continue
+            if grid2[i][j] == 1 and grid1[i][j] == 1:
+                if helper(i, j):
+                    print(f'{i} {j}')
+                    ans += 1
+    print(sorted(visit))
     return ans
 
-print(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
+
+print(countSubIslands(
+    [[1, 1, 1, 1, 0, 0], [1, 1, 0, 1, 0, 0], [1, 0, 0, 1, 1, 1], [1, 1, 1, 0, 0, 1], [1, 1, 1, 1, 1, 0],
+     [1, 0, 1, 0, 1, 0], [0, 1, 1, 1, 0, 1], [1, 0, 0, 0, 1, 1], [1, 0, 0, 0, 1, 0], [1, 1, 1, 1, 1, 0]],
+    [[1, 1, 1, 1, 0, 1], [0, 0, 1, 0, 1, 0], [1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [1, 1, 1, 0, 1, 0],
+     [0, 1, 1, 1, 1, 1], [1, 1, 0, 1, 1, 1], [1, 0, 0, 1, 0, 1], [1, 1, 1, 1, 1, 1], [1, 0, 0, 1, 0, 0]]))
