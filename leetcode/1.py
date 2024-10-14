d48116540eb962bf3ce07abb49cd9537ef1c7a35
Suchit Gupta <suchitgupta027@gmail.com>
2024-10-14 16:39:54 +0530

    simple updates

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,36 +1,23 @@
-def countSubIslands(grid1, grid2):
-    direc = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    ans = 0
-    visit = set()
-    m, n = len(grid1), len(grid1[0])
-    def helper(i, j):
-        visit.add((i, j))
-        ret = True
-        for x, y in direc:
-            a = i + x
-            b = j + y
-            if 0 <= a < m and 0 <= b < n and grid2[a][b] == 1 and (a, b) not in visit:
-                if grid1[a][b] == 0:
-                    helper(a, b)
-                    ret = False
-                else:
-                    ret = ret & helper(a, b)
-        return ret
+def smallestRange(nums):
+    store = []
+    n = len(nums)
+    for i in range(n):
+        for x in nums[i]:
+            store.append([x, i])
 
-    for i in range(m):
-        for j in range(n):
-            if (i, j) in visit:
-                continue
-            if grid2[i][j] == 1 and grid1[i][j] == 1:
-                if helper(i, j):
-                    print(f'{i} {j}')
-                    ans += 1
-    print(sorted(visit))
+    store.sort()
+
+    ans = [float('-inf'), float('inf')]
+    l = len(store)
+
+    for i in range(l):
+        visit, j = set(i for i in range(n)), i
+        while j < l and len(visit) > 0:
+            visit.discard(store[j][1])
+            j += 1
+        if j < l:
+            ans = min(ans, [store[i][0], store[j][0]], key=lambda x: x[1] - x[0])
+
     return ans
 
-
-print(countSubIslands(
-    [[1, 1, 1, 1, 0, 0], [1, 1, 0, 1, 0, 0], [1, 0, 0, 1, 1, 1], [1, 1, 1, 0, 0, 1], [1, 1, 1, 1, 1, 0],
-     [1, 0, 1, 0, 1, 0], [0, 1, 1, 1, 0, 1], [1, 0, 0, 0, 1, 1], [1, 0, 0, 0, 1, 0], [1, 1, 1, 1, 1, 0]],
-    [[1, 1, 1, 1, 0, 1], [0, 0, 1, 0, 1, 0], [1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [1, 1, 1, 0, 1, 0],
-     [0, 1, 1, 1, 1, 1], [1, 1, 0, 1, 1, 1], [1, 0, 0, 1, 0, 1], [1, 1, 1, 1, 1, 1], [1, 0, 0, 1, 0, 0]]))
+smallestRange([[4,10,15,24,26],[0,9,12,20],[5,18,22,30]])
