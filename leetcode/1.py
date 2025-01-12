e5b5b8b4f0719388241ba183f72033bcee1517ce
Suchit <110873739+Suchit027@users.noreply.github.com>
2025-01-12 14:09:04 +0530

    unnecessary changes

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,23 +1,26 @@
-def smallestRange(nums):
-    store = []
-    n = len(nums)
-    for i in range(n):
-        for x in nums[i]:
-            store.append([x, i])
+def canBeValid(s, locked):
+        op, cl = 0, 0
+        stack = []
+        for i in range(len(s)):
+            if s[i] == '(':
+                if locked[i] == 0:
+                    op += 1
+                stack.append(s[i])
+            else:
+                if not stack and locked[i] == '0':
+                    stack.append('(')
+                    cl += 1
+                elif not stack:
+                    return False
+                else:
+                    stack.pop()
+        if stack:
+            if op == cl:
+                return True
+            if len(stack) <= op:
+                return True
+            else:
+                return False
+        return True
 
-    store.sort()
-
-    ans = [float('-inf'), float('inf')]
-    l = len(store)
-
-    for i in range(l):
-        visit, j = set(i for i in range(n)), i
-        while j < l and len(visit) > 0:
-            visit.discard(store[j][1])
-            j += 1
-        if j < l:
-            ans = min(ans, [store[i][0], store[j][0]], key=lambda x: x[1] - x[0])
-
-    return ans
-
-print(smallestRange([[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]))
+canBeValid("))()))", "010100")
